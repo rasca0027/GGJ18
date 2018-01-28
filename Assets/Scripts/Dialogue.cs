c8da1faca94f2965d4e8a31c5d85c21b43586d5c
rasca0027 <rasca0027@gmail.com>
2018-01-28 13:53:10 -0500

    aaa

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -35,12 +35,18 @@ public class Dialogue : MonoBehaviour
 	public Dialog[] missions = new Dialog[3];
 	public GameObject myDia;
 	public GameObject customer;
+	public GameObject choicePanel;
 	public bool inMarket;
 	
 	private int progress;
 	private int curMissionNum;
 	private Dialog curMission;
-
+	private bool toggle;
+	private int interrogate;
+	
+	// TRY
+	//private List<List<string>> allMissions = new List<List<string>>();
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -49,31 +55,93 @@ public class Dialogue : MonoBehaviour
 		curMissionNum = 0;
 		curMission = missions[0];
 		inMarket = false;
+		toggle = false; // me
+		interrogate = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if ((curMissionNum + 1) % 3 == 0)
+		{
+			inMarket = false;
+			GetComponent<TimeController>().EndDay();
+		}
+
 		if (inMarket)
 		{
 			if (Input.GetMouseButtonDown(0))
 			{
-				progress += 1;
-				customer.active = false;
-				myDia.active = false;
-			}
+				if (progress == 3) // interrogate
+				{
+					toggle = !toggle;
+					interrogate += 1;
+					if (interrogate == curMission.interrogation.Length)
+					{
+						progress += 1; // to next level
+						interrogate = 0;
+						toggle = false;
+					}					
+				}
+				else if (progress == 4) // choice
+				{
+					// do something
+				}
+				else if (progress == 5) // end
+				{
+					// not die
+					progress = 0;
+					curMissionNum += 1;
+					curMission = missions[curMissionNum];
+				}
+				else
+				{
+					progress += 1;
+					customer.active = false;
+					myDia.active = false;
+				}
+				
+			} // end mouse down
 
+			
 			switch (progress)
 			{
 				case 0:
 					customer.active = true;
-					customer.GetComponent<Text>().text = missions[0].opening;
+					customer.transform.GetChild(0).GetComponent<Text>().text = curMission.opening;
 					break;
 				case 1:
 					myDia.active = true;
-					myDia.GetComponent<Text>().text = "What do you want?";
+					myDia.transform.GetChild(0).GetComponent<Text>().text = "What do you want?";
 					break;
-			}
+				case 2: // request
+					customer.active = true;
+					customer.transform.GetChild(0).GetComponent<Text>().text = curMission.request;
+					break;		
+				case 3: // interrogation
+					if (toggle)
+					{
+						// customer
+						myDia.active = false;
+						customer.active = true;
+						customer.transform.GetChild(0).GetComponent<Text>().text = curMission.interrogation[interrogate];
+					}
+					else
+					{
+						myDia.active = true;
+						customer.active = false;
+						myDia.transform.GetChild(0).GetComponent<Text>().text = curMission.interrogation[interrogate];
+					}
+					break;
+				case 4:
+					choicePanel.active = true;
+					// display choice
+					break;
+
+			} // end switch
+
+			
+			
 		}
 
 	}
@@ -82,6 +150,8 @@ public class Dialogue : MonoBehaviour
 
 	private void init()
 	{
+		
+
 		// TODO reead from json file
 		Dialog mission1 = new Dialog();
 		mission1.opening = "Hee-Haw! Out with your goods!";
@@ -93,6 +163,32 @@ public class Dialogue : MonoBehaviour
 		};
 		mission1.choices = new string[] { "Stress", "Decline" };
 		mission1.answer = 6;
+
 		missions[0] = mission1;
+		
+		// 2		
+		Dialog mission2 = new Dialog();
+		mission2.opening = "Hee-Haw! Out with your goods!";
+		mission2.request = "The forbidden fruit. My gateway to Valhalla. I want Joy!!!";
+		mission2.interrogation = new string[]
+		{
+			"Calm down man. I don’t have it. It takes ages to grow.",
+			"Out with it! Or I will kill you. You won’t be my first, and certainly not the last. "
+		};
+		mission2.choices = new string[] { "Stress", "Decline" };
+		mission2.answer = 6;
+
+		missions[1] = mission2;
+		missions[2] = mission2;
+
+	}
+
+	public void GiveStress()
+	{
+		// check inv
+		
+		// remove inv
+		
+		// check end
 	}
 }
